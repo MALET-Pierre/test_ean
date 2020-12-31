c72d176573bf6b12f0daf802b54f01122d5cf39c
MALET-Pierre <72810253+MALET-Pierre@users.noreply.github.com>
2020-12-31 14:22:18 +0100

    add gs1_labels

---
+++ suppl_delivery_gs1_labels/models/product_template.py
@@ -0,0 +1,31 @@
+# -*- coding: utf-8 -*-
+
+###################################################################################
+#
+#    Copyright (c) SUPPLiot GmbH.
+#
+#    This file is part of SUPPLiot GS1 SSCC Labels module
+#    (see https://suppliot.eu).
+#
+#    See LICENSE file for full copyright and licensing details.
+#
+###################################################################################
+
+
+from odoo import api, fields, SUPERUSER_ID, http, models, _
+from odoo.exceptions import UserError
+
+from ..common import get_gs1_barcode
+
+
+class ProductTemplate(models.Model):
+    _inherit = 'product.template'
+
+    def create_barcode_number(self):
+        for prod in self:
+            if prod.barcode and len(prod.barcode) > 0:
+                raise UserError(_('Product %s already has an assigned barcode [%s]. If you are sure, you want to set a new barcode, please empty the current barcode field.') % (prod.display_name, prod.barcode))
+
+            generated_barcode = get_gs1_barcode(self.env)
+            if generated_barcode:
+                prod.barcode = generated_barcode
